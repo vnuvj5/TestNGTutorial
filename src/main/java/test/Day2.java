5444e0444e4da86075392521694180b861846f56
vnu <vnuvj5@gmail.com>
2026-07-27 13:09:54 -0400

    Day2 Change

--- src/main/java/test/Day2.java
+++ src/main/java/test/Day2.java
@@ -9,7 +9,7 @@ public class Day2 {
     @Test(groups={"Smoke"})
     public void ploan()
     {
-        System.out.println("good");
+        System.out.println("good Day2");
     }
 
     @BeforeTest
