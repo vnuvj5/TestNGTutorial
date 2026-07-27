c63e0e141d82b99ba951d6d18cf715c87d8c1b76
vnu <vnuvj5@gmail.com>
2026-07-27 11:42:17 -0400

    New TestNG project

---
+++ src/main/java/test/Listeners.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed: " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped: " + result.getName());
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Test Suite Started");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Test Suite Finished");
+    }
+}
